bee185598d6784fcbbc68b7ad87bbf27a03efa05
pavelfromua <50228485+pavelfromua@users.noreply.github.com>
2020-09-17 13:36:45 +0300

    hw 40 sceleton

---
+++ src/main/java/com/dev/cinema/dao/impl/CinemaHallDaoImpl.java
@@ -0,0 +1,19 @@
+package com.dev.cinema.dao.impl;
+
+import com.dev.cinema.dao.CinemaHallDao;
+import com.dev.cinema.lib.Dao;
+import com.dev.cinema.model.CinemaHall;
+import java.util.List;
+
+@Dao
+public class CinemaHallDaoImpl implements CinemaHallDao {
+    @Override
+    public CinemaHall add(CinemaHall cinemaHall) {
+        return null;
+    }
+
+    @Override
+    public List<CinemaHall> getAll() {
+        return null;
+    }
+}
