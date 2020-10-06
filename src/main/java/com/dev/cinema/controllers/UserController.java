bc91ab682baa18f81d45ab5eefdc0c9b12867ca4
pavelfromua <50228485+pavelfromua@users.noreply.github.com>
2020-10-06 18:52:21 +0300

    47 start

--- src/main/java/com/dev/cinema/controllers/UserController.java
+++ src/main/java/com/dev/cinema/controllers/UserController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/users")
 //user/inject
 //user/{userId}
 //user/
